eebffa091d3dddada126ccf65463fbb3371878d0
Zaida Hammel <zzaida.hammel@mths.ca>
2022-10-17 15:00:06 +0000

    added python file

---
+++ decimals.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Zaida Hammel
+# Created on: Oct 2022
+# This program adds the numbers 0.1 and 0.2 in binary
+
+
+def main():
+    # this function adds the numbers 0.1 and 0.2 in binary
+
+    # input
+    print("This program adds the numbers 0.1 and 0.2.")
+
+    # process
+    answer = 0.1 + 0.2
+
+    # output
+    print("The sum of 0.1 + 0.2 = {0}".format(answer))
+
+    print("")
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
